6181cede05835a821d0a0f4ccb8c6a55b5099dc9
Paco <vyachhw@gmail.com>
2023-05-29 22:46:34 +0300

    Final-Version

---
+++ FirstTask/Model/NumberSequencePrinter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstTaskProject.Model
+{
+    public class NumberSequencePrinter
+    {
+        public static void Start()
+        {
+            Console.Write("Введите конечное значение (положительное, не буква): ");
+            if (int.TryParse(Console.ReadLine(), out int endValue) && endValue > 0) // Проверка на корректное введение значения пользователем.
+            {
+                string result = CalculateValue(endValue); // Создаем строку
+                Print(result); // Выводим ее
+            }
+            else
+            {
+                Console.WriteLine("Введите корректное значение. " +
+                    "Число должно быть меньше чем 2147483647 и больше нуля.");
+            }
+        }
+        public static void Print(string value)
+        {
+            Console.WriteLine("Ваша последовательность: " + value);
+        }
+        /// <summary>
+        /// Возвращает последовательность чисел от 1 до указанного значения.
+        /// </summary>
+        /// <param name="finalValue">Конечное значение.</param>
+        public static string CalculateValue(int finalValue)
+        {
+            return string.Join(", ", Enumerable.Range(1, finalValue));
+        }
+
+    }
+}
---
+++ FirstTask/Model/SquareWithSpacePrinter.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstTaskProject.Model
+{
+    public class SquareWithSpacePrinter
+    {
+
+        public static void Start()
+        {
+            try
+            {
+                Console.Write("Введите нечетное число N: ");
+                if (int.TryParse(Console.ReadLine(), out int oddNumber))
+                {
+                    string result = MakeSquare(oddNumber);
+                    if (result != "")
+                    {
+                        Print(result);
+                    }
+                }
+                else { Console.WriteLine("Вы ввели четное N."); }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Ошибка:" + ex.Message);
+            }
+        }
+
+        public static void Print(string value)
+        {
+            Console.WriteLine("Ваша фигура: \n" + value);
+        }
+        /// <summary>
+        /// Создает квадрат, состоящий из символа «#», с «дыркой» в центре квадрата.
+        /// </summary>
+        /// <param name="oddInputValue">Нечетное значение.</param>
+        public static string MakeSquare(int oddInputValue)
+        {
+            if (oddInputValue % 2 == 0) // If oddInputValue is an even number, exit the function.
+            {
+                Console.WriteLine("Вы ввели четное N.");
+                return "";
+            }
+            //string result = "";  // Увидел ощутимую разницу в скорости работы между string и StringBuilder.
+            StringBuilder result = new();
+            for (int row = 1; row <= oddInputValue; row++)
+            {
+                for (int col = 1; col <= oddInputValue; col++)
+                {
+                    if (row == oddInputValue / 2 + 1 && col == oddInputValue / 2 + 1) /*result += ' '*/ result.Append(' ');
+                    else /*result += '#'*/result.Append('#');
+                }
+                //result += '\n';
+                result.Append('\n');
+            }
+            return result.ToString();
+        }
+
+    }
+}
