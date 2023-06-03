bcde16d1cb03a7b7348d8b3c768142b8c97311a0
Paco <vyachhw@gmail.com>
2023-06-03 17:19:22 +0300

    New Commit
    Square With Space Edit

--- BMIWebApi/Migrations/20230602142331_BMIThreshold.Designer.cs
+++ BMIWebApi/Migrations/20230602142331_BMIThreshold.Designer.cs
@@ -3,6 +3,7 @@ using BMIWebApi.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BMIWebApi.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20230602142331_BMIThreshold")]
+    partial class BMIThreshold
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ BMIWebApi/Migrations/20230602142331_BMIThreshold.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BMIWebApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class BMIThreshold : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Index",
+                table: "BMIThresholds",
+                newName: "IndexTo");
+
+            migrationBuilder.AddColumn<double>(
+                name: "IndexFrom",
+                table: "BMIThresholds",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IndexFrom",
+                table: "BMIThresholds");
+
+            migrationBuilder.RenameColumn(
+                name: "IndexTo",
+                table: "BMIThresholds",
+                newName: "Index");
+
+        }
+    }
+}
